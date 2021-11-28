87941d48ec9467d86c63a31213fdd8b5c39eb33c
diosurandito <diosurandito@gmail.com>
2021-11-28 16:00:42 +0700

    07.Context with Timeout

--- context_test.go
+++ context_test.go
@@ -73,6 +73,27 @@ func CreateCounterNew(ctx context.Context) chan int {
 	}()
 	return destination
 }
+
+func CreateCounterTimeout(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+				time.Sleep(1 * time.Second)
+			}
+
+		}
+	}()
+	return destination
+}
 func TestContextWithCancel(t *testing.T) {
 	fmt.Println("Total Goroutine", runtime.NumGoroutine())
 
@@ -103,5 +124,20 @@ func TestContextWithCancelWithoutLeak(t *testing.T) {
 	}
 	cancel() // mengirim sinyal cancel ke context
 	time.Sleep(2 * time.Second)
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
+
+func TestContextWithTimeout(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithTimeout(parent, 5*time.Second)
+	defer cancel() // mengirim sinyal cancel ke context
+	destination := CreateCounterTimeout(ctx)
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
+
 	fmt.Println("Total Goroutine", runtime.NumGoroutine())
 }
